40bc41f67c1bca254f399e44ff790003bec81555
Jadden Picardal <jaddenki@gmail.com>
2025-05-27 22:50:35 -0700

    added the cornfields

--- site/src/components/AnimatedText.tsx
+++ site/src/components/AnimatedText.tsx
@@ -4,9 +4,8 @@ const REDACTION_WEIGHTS = [10, 20, 35, 50, 70, 100];
 
 // Map of original words to their alternates
 const WORD_ALTERNATES: { [key: string]: string[] } = {
-  "studying": ["suffering through", "conquering", "surviving", "speedrunning"],
-  "computer engineering": ["machine feelings", "minecraft redstone"],
-  "engineering": ["magic", "shenanigans"],
+  "studying": ["suffering through", "conquering", "surviving", "speedrunning", "frolicking through"],
+  "computer engineering": ["minecraft redstone", "cornfields"],
   "creative tech, motion, and the spaces in between" : ["not blowing up my computer", "pretty math and spicy metal","googling until it works"],
 
 };
